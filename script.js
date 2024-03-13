83c78f2ea1f7b3c8cb17087917b3f079d350d10c
OddestOfAllDean <alideanbaskoro@gmail.com>
2024-03-13 12:05:40 +0700

    html asu pake ngga bisa segala di laptop

---
+++ script.js
@@ -0,0 +1,16 @@
+
+AOS.init();
+const header=document.querySelector("header");
+const scroll_btn=document.querySelector("#scroll");
+
+window.addEventListener("scroll",function() {
+    header.classList.toggle("sticky",window.scrollY > 10);
+    scroll_btn.classList.toggle("active",window.scrollY > 50);
+});
+
+let nav=document.querySelector("#navlist")
+let bars=document.querySelector("#menu-icon")
+
+bars.addEventListener("click",()=>{
+    nav.classList.toggle("show_nav")
+})  
